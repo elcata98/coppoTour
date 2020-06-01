d40202f01591f96e69eb771c142a864750393cc5
Francisco Martinez Posadas <elcata98@gmail.com>
2020-06-02 08:27:00 +0900

    folder structure refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
