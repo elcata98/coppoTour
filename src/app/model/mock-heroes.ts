60393a1f0790e6214c3f5078be208b94cbb2710d
Francisco Martinez Posadas <elcata98@gmail.com>
2020-06-02 19:31:45 +0900

    details

---
+++ src/app/model/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Jo' },
+    { id: 12, name: 'Neko' },
+    { id: 13, name: 'Dejarik' },
+    { id: 14, name: 'Gasso' },
+    { id: 15, name: 'Airuntx' }
+];
