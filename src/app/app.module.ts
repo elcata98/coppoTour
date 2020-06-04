be12c25c9bd270205a255a9e945e21245cb1ec21
Francisco Martinez Posadas <elcata98@gmail.com>
2020-06-04 18:05:31 +0900

    crud wo search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,9 +23,17 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     DashboardComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/hero-detail/hero-detail.component.ts
+++ src/app/components/hero-detail/hero-detail.component.ts
@@ -29,11 +29,18 @@ export class HeroDetailComponent implements OnInit {
   loadHero(): void {
 
     const id = +this.route.snapshot.paramMap.get('id');
+    
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
